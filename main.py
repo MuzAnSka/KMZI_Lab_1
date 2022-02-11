2acaddb0fe96fbae4dc9ffceedb8ade09933de3f
nerdoweirdo <63196427+nerdoweirdo@users.noreply.github.com>
2022-02-11 16:44:12 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 
 
-lrus = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х',
+lrus = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х',
         'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
-brus = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х',
+brus = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х',
         'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я']
 lend = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
         'w', 'x', 'y', 'z']
@@ -48,13 +48,12 @@ def decryptCaesar(msg, shift):
                 else:
                         ret += x
 
-
         return ret
 
 def dec(msg):
-        q = open('abc.txt', 'w', encoding='utf-8')
+        q = open('decoded_b_f.txt', 'w', encoding='utf-8')
         ret = ""
-        shift = 0
+        shift = 1
         for shift in range(len(lrus)):
                 for x in msg:
                         if x in lrus:
@@ -72,17 +71,32 @@ def dec(msg):
                         else:
                                 ret += x
 
-                q.write('ключ = %s\n %s \n' % (shift, ret))
+                q.write('Shift = %s\n %s \n' % (shift, ret))
                 ret = ""
         return ret
 
-f = open('a.txt', encoding='utf-8')
-b = open('ab.txt', 'w', encoding='utf-8')
 
-a = f.read()
-c = encryptCaesar(a, 10)
-print(encryptCaesar(a, 10))
-print(decryptCaesar(c, 10))
-s = dec(c)
-b.write(c)
-
+print("Choose a type of the process:\n1. Encyption\n2. Decryption\n3. Decryption (brute force attack)")
+type = int(input())
+if type == 1:
+        f = open('sample.txt', encoding='utf-8')
+        b = open('encoded.txt', 'w', encoding='utf-8')
+        k = open('shift.txt', encoding='utf-8')
+        sh = int(k.read())
+        a = f.read()
+        c = encryptCaesar(a, sh)
+        b.write(c)
+elif type == 2:
+        f = open('encoded.txt', encoding='utf-8')
+        b = open('decoded.txt', 'w', encoding='utf-8')
+        k = open('shift.txt', encoding='utf-8')
+        sh = int(k.read())
+        a = f.read()
+        c = decryptCaesar(a, sh)
+        b.write(c)
+elif type == 3:
+        f = open('encoded.txt', encoding='utf-8')
+        a = f.read()
+        s = dec(a)
+else:
+        print('Oops! Start again')
